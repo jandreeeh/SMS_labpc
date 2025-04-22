5c0739af9951864a239904096f0256b97ba38e3f
Jandre <leejohnandrei68@gmail.com>
2025-04-22 15:14:02 +0800

    Added AppDbSeed and changed framwork to EntityFrameworkCore

--- SMS.Infastructure/Data/AppDbContext.cs
+++ SMS.Infastructure/Data/AppDbContext.cs
@@ -1,14 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using SMS.Domain;
 
 namespace SMS.Infastructure.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<AppUser>
     {
+        public AppDbContext() : base() { }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=db_students;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
             base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            AppDbSeed.SeedUsers(builder);
+            base.OnModelCreating(builder);
         }
 
         public DbSet<Student> Students { get; set; }
---
+++ SMS.Infastructure/Data/AppDbSeed.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SMS.Domain;
+
+namespace SMS.Infastructure.Data
+{
+    public class AppDbSeed
+    {
+        public static void SeedUsers(ModelBuilder builder)
+        {
+            var passwordHasher = new PasswordHasher<AppUser>();
+
+            var appUser = new AppUser
+            {
+                Id = Guid.NewGuid().ToString(),
+                PasswordHash = passwordHasher.HashPassword(null, "admin"),
+            };
+
+            builder.Entity<AppUser>().HasData(appUser); 
+        }
+    }
+}
